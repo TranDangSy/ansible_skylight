3c396596e231bea7d0a26d40dc699ab851c2b811
TranDangSy <dangsy1998@gmail.com>
2024-02-20 18:09:21 +0700

    Create fw_back.py

---
+++ ansible/fw_back.py
@@ -0,0 +1,23 @@
+from netmiko import ConnectHandler
+from datetime import datetime
+
+my_fw = {'host': 'Skylight_FW01_G',
+         'device_type': 'fortinet',
+         'ip': '10.1.1.1',
+         'username': 'backup_user',
+         'password': 'buncha11a@',
+         'port': '2222'
+}
+
+now = datetime.now()
+
+net_connect = ConnectHandler(**my_fw)
+net_connect.enable()
+
+output = net_connect.send_command_timing('show full-configuration', delay_factor=4)
+
+current_time = now.strftime('%d_%m_%Y')
+
+with open("fortigate_backups/" + my_fw['host'] + '_' + current_time + ".cfg", 'w+') as f:
+    for line in output:
+        f.write(line)
