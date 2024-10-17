a08d501c4604d69836f4b1d560d63abfeb38225a
TranDangSy <dangsy1998@gmail.com>
2024-10-17 17:04:40 +0700

    update backup firewall by ansible

--- ansible/fw_back.py
+++ ansible/fw_back.py
@@ -1,6 +1,5 @@
-from netmiko import ConnectHandler
+from netmiko import ConnectHandler # type: ignore
 from datetime import datetime
-
 my_fw = {'host': 'Skylight_FW01_G',
          'device_type': 'fortinet',
          'ip': '10.1.1.1',
@@ -8,7 +7,6 @@ my_fw = {'host': 'Skylight_FW01_G',
          'password': 'buncha11a@',
          'port': '2222'
 }
-
 now = datetime.now()
 
 net_connect = ConnectHandler(**my_fw)
